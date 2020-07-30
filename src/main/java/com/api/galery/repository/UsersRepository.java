acb5945cbd3ec30e00fdf79a55de5f5e512e6402
Nieradka21 <67032746+Nieradka21@users.noreply.github.com>
2020-07-30 10:34:50 -0300

    30/07/2020

--- src/main/java/com/api/galery/repository/UsersRepository.java
+++ src/main/java/com/api/galery/repository/UsersRepository.java
@@ -1,11 +1,18 @@
 package com.api.galery.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.api.galery.model.Users;
 
-public interface UsersRepository extends JpaRepository<Users, Long>{
+public interface UsersRepository  extends  JpaRepository<Users, Long>{
 	
 	Users findById(long id);
+	
+	@Query(value = "SELECT u FROM Users u WHERE u.name IN :names")
+	Users findByName(@Param("names")String name); 
 
 }
+
+
--- src/main/java/com/api/galery/resources/UsersResource.java
+++ src/main/java/com/api/galery/resources/UsersResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +27,17 @@ public class UsersResource {
 		
 		return usersRepository.findAll();
 	}
+	
+	@GetMapping("/users/{id}")
+	public Users buscarUser(@PathVariable(value="id") long id) {
+		return usersRepository.findById(id);
+			}
+	
+@PostMapping("/users/user/{name}")
+	public Users buscarUsuario(@PathVariable(value = "name") String n) {
+		return usersRepository.findByName(n);
+	}
+	
+	
+	
 }
