1b392e8a1a7265cc5e6ae5b774e23dfd8c285338
Wrdz - Ivan Mateus <ter.imateus@restoque.com.br>
2020-08-09 13:31:57 -0300

    add metodo excluir e editar

--- src/main/java/com/api/galery/repository/UsersRepository.java
+++ src/main/java/com/api/galery/repository/UsersRepository.java
@@ -6,17 +6,16 @@ import org.springframework.data.repository.query.Param;
 
 import com.api.galery.model.Users;
 
-
 public interface UsersRepository extends JpaRepository<Users, Long> {
 
 	Users findById(long id);
-	
+
+	Users deleteById(long id);
+
 	@Query(value = "SELECT u.name,u.pass FROM Users u WHERE u.name = :names and u.pass = :passw")
 	Users findByNameAndPass(@Param("names") String name, @Param("passw") String pass);
 
 	@Query(value = "SELECT u.pass FROM Users u WHERE u.pass = :passw")
 	Users findByPass(@Param("passw") String pass);
 
-
 }
-
--- src/main/java/com/api/galery/resources/UsersResource.java
+++ src/main/java/com/api/galery/resources/UsersResource.java
@@ -4,14 +4,17 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -41,6 +44,18 @@ public class UsersResource {
 		return usersRepository.save(users);
 	}
 
+	@PutMapping("/user")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Users editarUsuario(@RequestBody @Valid Users users) {
+		return usersRepository.save(users);
+	}
+
+	@DeleteMapping("/user/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Users deletarUsuario(@PathVariable(value = "id") long id) {
+		return usersRepository.deleteById(id);
+	}
+	
 	@PostMapping("/login")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response loginAutenticar(@RequestBody String t) {
